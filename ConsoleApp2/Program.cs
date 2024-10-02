4c0c49360c82a5d1c7cec661959c4c5aef646ae8
begüm doğanay <86783970+begumdoganay@users.noreply.github.com>
2024-10-02 11:45:06 +0300

    denemelerdenemeler
    devamını getirmeye çalışıyorum

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class MyClass
+{
+    static void Main()
+    {
+        int tekSayıToplamı = 0;
+        int çiftSayıToplamı = 0;
+
+        for (int i = 0; i <= 120; i++)
+        {
+            if (i % 2 == 0)
+            {
+                çiftSayıToplamı += i;
+            }
+            else
+            {
+                tekSayıToplamı += i;
+            }
+        }
+        Console.WriteLine("1 ile 120 arasındaki çift sayıların toplamı:    " + çiftSayıToplamı);
+        Console.WriteLine("1 ile 120 arasındaki tek sayıların toplamı :    " +  tekSayıToplamı);
+
+        Console.ReadKey();
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            Console.WriteLine($" Şuan bu sayıdasın: {i}");
+        }
+    }
+}
